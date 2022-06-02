f8adc74242501c22d155ea9ec422332d566ec558
Kondwani Kamundi <kkamundi@gmail.com>
2022-06-02 10:22:34 -0700

    C program that prints exactly

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * print f funciton
+ * main - main block
+ * Return: 0
  */
-int main (void)
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
